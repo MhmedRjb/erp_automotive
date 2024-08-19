baa37f7a6964c23cc5a213cf22994c79a6bad706
MhmedRjb <mohamed2elfateh@gmail.com>
2024-08-19 14:26:21 +0300

    feat:add the serve script to allow customes card to assign to seril noa

--- erp_automotive/erp_automotive/doctype/customs_card_receipt/customs_card_receipt.py
+++ erp_automotive/erp_automotive/doctype/customs_card_receipt/customs_card_receipt.py
@@ -3,7 +3,20 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
 
 class customscardreceipt(Document):
-	pass
+
+	
+	def validate(self):
+		for item in self.items:
+			if not frappe.db.exists("Serial No", item.serial_no):
+				frappe.throw(f"Serial No {item.serial_no} does not exist")
+
+		for item in self.items:
+			if not item.customs_card:
+				frappe.throw(f"Customs Card {item.serial_no} does not exist")				
+				
+	def on_submit(self):
+		for item in self.items:
+			frappe.db.set_value("Serial No", item.serial_no, "customs_card", item.customs_card)
