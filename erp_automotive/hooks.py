b29f96014899581cbef7ba6bd204303b8ee9c1cc
mhmedrjb <mohamed2elfateh@gmail.com>
2024-08-22 15:17:25 +0300

    feat: Refactor Sales Order Item form to include custom_add_serials function

--- erp_automotive/hooks.py
+++ erp_automotive/hooks.py
@@ -28,8 +28,9 @@ app_license = "MIT"
 
 # include js in doctype views
 doctype_js = {
-                "Purchase Receipt" : "public/js/purchase_receipt.js"
-    }
+                "Purchase Receipt" : "public/js/purchase_receipt.js",
+                "Sales Order" : "public/js/sales_order_reservations.js"
+                }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -233,5 +234,8 @@ fixtures = [
             ]
         ]
     }
+,{
+        "dt": "Custom Field",
+}
 ]
 
---
+++ erp_automotive/public/js/sales_order_reservations.js
@@ -0,0 +1,79 @@
+frappe.ui.form.on('Sales Order', {
+    refresh: function(frm) {
+        console.log("tesst"),
+        frm.set_query("custom_serial_no", "items", function (doc, cdt, cdn) {
+          return {
+            "filters": {
+              "item_name": "item_code"
+            },
+          };
+        });
+    }
+});
+
+frappe.ui.form.on('Sales Order Item', {
+    custom_add_serials: async   function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];        
+        let d = new frappe.ui.Dialog({
+            title: 'Reserved Serials',
+            fields: [
+                {
+                    label: 'Item Code',
+                    fieldname: 'item_code',
+                    fieldtype: 'Data',
+                    read_only: 1,
+                    default: row.item_code
+                },
+                {
+                    label: 'Warehouse',
+                    fieldname: 'warehouse',
+                    fieldtype: 'Data',
+                    read_only: 1,
+                    default: row.warehouse
+                },
+                {
+                    label: 'Quantity',
+                    fieldname: 'qty',
+                    fieldtype: 'Data',
+                    read_only: 1,
+                    default: row.qty
+                },
+                {
+                    label: 'Serial Numbers',
+                    fieldname: 'serial_numbers',
+                    fieldtype: 'Table',
+                    fields: [
+                        {
+                            fieldtype: 'Link',
+                            fieldname: 'serial_no',
+                            label: 'Serial No',
+                            options: 'Serial No',
+                            get_query: () => {
+                                return {
+                                    filters: {
+                                        item_code: row.item_code
+                                    }
+                                };
+                            }
+                        },
+                        {
+                            fieldtype:'Data',
+                            fieldname :'test',
+                            label: 'tete No'
+
+                        }
+                    ]
+                }
+            ],
+            primary_action_label: 'Save',
+            primary_action(values) {
+                let serial_numbers = values.serial_numbers.map(row => row.serial_no);
+                console.log(serial_numbers);
+                d.hide();
+            }
+        });
+
+        // Show the dialog
+        d.show();
+    }
+});
