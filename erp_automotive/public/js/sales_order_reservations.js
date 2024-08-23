490b9d2f864902d0114856a9cd869b2345946ce6
mhmedrjb <mohamed2elfateh@gmail.com>
2024-08-23 16:22:44 +0300

    Refactor add seril no to the pop up page

--- erp_automotive/public/js/sales_order_reservations.js
+++ erp_automotive/public/js/sales_order_reservations.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Sales Order', {
     refresh: function(frm) {
-        console.log("tesst"),
+        console.log("tesst");
         frm.set_query("custom_serial_no", "items", function (doc, cdt, cdn) {
           return {
             "filters": {
@@ -11,69 +11,160 @@ frappe.ui.form.on('Sales Order', {
     }
 });
 
-frappe.ui.form.on('Sales Order Item', {
-    custom_add_serials: async   function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];        
-        let d = new frappe.ui.Dialog({
-            title: 'Reserved Serials',
-            fields: [
-                {
-                    label: 'Item Code',
-                    fieldname: 'item_code',
-                    fieldtype: 'Data',
-                    read_only: 1,
-                    default: row.item_code
-                },
-                {
-                    label: 'Warehouse',
-                    fieldname: 'warehouse',
-                    fieldtype: 'Data',
-                    read_only: 1,
-                    default: row.warehouse
-                },
-                {
-                    label: 'Quantity',
-                    fieldname: 'qty',
-                    fieldtype: 'Data',
-                    read_only: 1,
-                    default: row.qty
-                },
-                {
-                    label: 'Serial Numbers',
-                    fieldname: 'serial_numbers',
-                    fieldtype: 'Table',
-                    fields: [
-                        {
-                            fieldtype: 'Link',
-                            fieldname: 'serial_no',
-                            label: 'Serial No',
-                            options: 'Serial No',
-                            get_query: () => {
-                                return {
-                                    filters: {
-                                        item_code: row.item_code
-                                    }
-                                };
-                            }
-                        },
-                        {
-                            fieldtype:'Data',
-                            fieldname :'test',
-                            label: 'tete No'
+frappe.ui.form.on("Sales Order Item", "custom_add_serials", async function(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+    console.log('Row:', row);
 
-                        }
-                    ]
-                }
-            ],
-            primary_action_label: 'Save',
-            primary_action(values) {
-                let serial_numbers = values.serial_numbers.map(row => row.serial_no);
-                console.log(serial_numbers);
-                d.hide();
-            }
-        });
-
-        // Show the dialog
-        d.show();
+    // Check if custom_serial_no_saver is empty
+    if (!row.custom_serial_no_saver || row.custom_serial_no_saver.length === 0) {
+        // Scenario 1: No data in custom_serial_no_saver
+        openSerialNumberDialog(frm, row);
+    } else {
+        // Scenario 2: Data already in custom_serial_no_saver
+        openSerialNumberDialogWithExistingData(frm, row);
     }
 });
+
+// Function to open the dialog and add serial numbers
+function openSerialNumberDialog(frm, row) {
+    let d = new frappe.ui.Dialog({
+        title: 'Reserved Serials',
+        size :'extra-large',
+        fields: [
+            {
+                label: 'Item Code',
+                fieldname: 'item_code',
+                fieldtype: 'Data',
+                read_only: 1,
+                default: row.item_code
+            },
+            {
+                label: 'Warehouse',
+                fieldname: 'warehouse',
+                fieldtype: 'Data',
+                read_only: 1,
+                default: row.warehouse
+            },
+            {
+                label: 'Quantity',
+                fieldname: 'qty',
+                fieldtype: 'Data',
+                read_only: 1,
+                default: row.qty
+            },
+            {
+                fieldname: "items",
+                fieldtype: "Table",
+                label: __("Items to Reserve"),
+                size:"extra-large",
+                data: [],
+                fields: [
+                    {
+                        fieldname: "serial_no",
+                        fieldtype: "Link",
+                        label: __("serial no"),
+                        options: "Serial No",
+                        in_list_view: 1,
+                        get_query: () => {
+                            return {
+                                filters: {
+                                    item_code: row.item_code
+                                }
+                            };
+                        }
+                    },
+                ],
+            },
+        ],
+        primary_action_label: 'Submit',
+        primary_action(values) {
+            console.log('Dialog Values:', values);
+            let items = d.get_values().items;
+            console.log(items);
+            // Make it in text format
+            let serials = '';
+            items.forEach(function(item) {
+                serials += item.serial_no + '\n';
+            });
+            row.custom_serial_no_saver = serials;
+            frm.refresh_field('items');
+            d.hide();  
+            console.log(row.custom_serial_no_saver);
+        }
+    });
+    d.show();
+}
+
+// Function to open the dialog with existing serial numbers
+function openSerialNumberDialogWithExistingData(frm, row) {
+    let existingSerials = row.custom_serial_no_saver.split('\n').filter(Boolean).map(serial => ({ serial_no: serial }));
+    
+    let d = new frappe.ui.Dialog({
+        title: 'Reserved Serials',
+        size :'extra-large',
+        fields: [
+            {
+                label: 'Item Code',
+                fieldname: 'item_code',
+                fieldtype: 'Data',
+                read_only: 1,
+                default: row.item_code
+            },
+            {
+                label: 'Warehouse',
+                fieldname: 'warehouse',
+                fieldtype: 'Data',
+                read_only: 1,
+                default: row.warehouse
+            },
+            {
+                label: 'Quantity',
+                fieldname: 'qty',
+                fieldtype: 'Data',
+                read_only: 1,
+                default: row.qty
+            },
+            {
+                fieldname: "items",
+                fieldtype: "Table",
+                label: __("Items to Reserve"),
+                size:"extra-large",
+                data: existingSerials,
+                fields: [
+                    {
+                        fieldname: "serial_no",
+                        fieldtype: "Link",
+                        label: __("serial no"),
+                        options: "Serial No",
+                        in_list_view: 1,
+                        get_query: () => {
+                            return {
+                                filters: {
+                                    item_code: row.item_code
+                                }
+                            };
+                        }
+                    },
+                ],
+            },
+        ],
+        primary_action_label: 'Submit',
+        primary_action(values) {
+            console.log('Dialog Values:', values);
+            let items = d.get_values().items;
+            console.log(items);
+            // Make it in text format
+            let serials = '';
+            items.forEach(function(item) {
+                serials += item.serial_no + '\n';
+            });
+            row.custom_serial_no_saver = serials;
+            frm.refresh_field('items');
+            d.hide();  
+            console.log(row.custom_serial_no_saver);
+        }
+    });
+    d.show();
+}
+
+let items_serials = {};
